0e25a7589932c3881ced68f4f1a81f3c696d13f1
naokisz <naokihirokitomoki@gmail.com>
2015-08-05 18:29:37 +0900

    Changes to be committed: 	modified:   flops 	modified:   flops.c
    コメント追加、確認プロンプト追加

--- flops.c
+++ flops.c
@@ -13,6 +13,10 @@ int main(void) {
 
 	printf("スパコンと計算能力比べ!!!\n");
 
+	printf("測定を開始するためにはエンターキーを押してください。＞");
+	rewind(stdin);
+	getchar();
+
 	gettimeofday(&start, NULL); // 始めた時間取得
 
 
@@ -38,8 +42,8 @@ int main(void) {
 	printf("正解数　%d問！！\n時間　%.15lf秒\nあなたのflops　%.15lfFLOPS！！\n東北大のスパコンの1/%ld\n京の1/%ld倍\n", 
 		count,((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)*1.0E-6), // countで正解数表示。時間は秒単位でもマイクロ秒単位でもそれぞれ終わった時間から始めた時間を引いて,それぞれを足す。マイクロ秒の方の*1.0E-6は意味不明。
 		((double)count) / ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)*1.0E-6), // １秒あたりの正解数　＝　FLOPS
-		(long)(707000000000000 / (((double)count) / ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)*1.0E-6))),
-		(long)(10000000000000000 / (((double)count) / ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)*1.0E-6))));
+		(long)(707000000000000 / (((double)count) / ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)*1.0E-6))), // 東北大のスパコンとの比較
+		(long)(10000000000000000 / (((double)count) / ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)*1.0E-6)))); // 京との比較
 	return 0;
 }
 
