7d2a324f28ec5ef826e9aeb9dad31fd38765a697
naokisz <naokihirokitomoki@gmail.com>
2015-08-05 15:00:38 +0900

    new file:   flops 	modified:   flops.c
    flops.c 最適化
    flops 実行ファイルも上げておいた

--- flops.c
+++ flops.c
@@ -5,7 +5,7 @@
 
 int main(void) {
 
-	int n, count;
+	int n, count = 0;
 	double a, b, answer[10], player[10], flops;
 	struct timeval start, end;
 
@@ -30,15 +30,15 @@ int main(void) {
 		scanf("%3lf", &player[n]);
 		while(getchar() != '\n') { }
 
+		if(fabs( answer[n] - player[n]) < 0.00000000000001 ) count++;
+
 	}
 
 	gettimeofday(&end, NULL);
 
-	for( n = 0, count = 0; n < 10; n++) if(fabs( answer[n] - player[n]) < 0.00000000000001 ) count++;
-
-	flops = ((double)count) / ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)*1.0E-6);
-
-	printf("正解数　%d問！！\n時間　%.15lf秒\nあなたのflops　%.15lfFLOPS！！\n", count,((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)*1.0E-6), flops);
+	printf("正解数　%d問！！\n時間　%.15lf秒\nあなたのflops　%.15lfFLOPS！！\n", 
+		count,((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)*1.0E-6),
+		((double)count) / ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)*1.0E-6));
 
 	return 0;
 }
