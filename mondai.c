d5218d4a09e82a5d1621478ee8af588d19107c53
naokisz <naokihirokitomoki@gmail.com>
2015-08-14 16:07:25 +0900

    コメント追加。

--- mondai.c
+++ mondai.c
@@ -2,28 +2,37 @@
 #include <stdlib.h>
 #include <math.h>
 
-int mondai(int mondaisu) {
+/* int mondai
 
-	int n, count;
-	double a, b, player;
+引数　int型　問題数
+返り値　int型　回答数
 
-	srand(time(NULL));
+小数第一位の数を乱数で作って、問題を出し、正解数を数える。
 
-	for (n = 0; n < mondaisu; n++) {
+*/
 
-		a = rand()%49+1;
+int mondai(int mondaisu) { // 問題数を引数として持つ。
+
+	int n, count; // nはforのカウント、countは正解数。
+	double a, b, player; // a、bは問題。playerは入力されたもの。
+
+	srand(time(NULL)); // 乱数の種まき　mainでやるべきだろうけれど面倒だからここに書いた。
+
+	for (n = 0; n < mondaisu; n++) { // mondaisuの分だけ問題作成
+
+		a = rand()%49+1; // 乱数を作る。答えを１０や０にしたくないから０．１〜４．９までの乱数に制限。
 		b = rand()%49+1;
 
-		a /= 10;
+		a /= 10; // 小数にするため１０で割る
 		b /= 10;
 
-		printf("%.1lf+%.1lf=", a, b);
-		scanf("%3lf", &player);
-		while(getchar() != '\n') {  }
+		printf("%.1lf+%.1lf=", a, b); // 問題表示
+		scanf("%3lf", &player); // プレイヤー答え入力
+		while(getchar() != '\n') {  } // 余計な値捨て
 
-		if(fabs( (a + b) - player) < 0.00000000000001 ) count++;
+		if(fabs( (a + b) - player) < 0.00000000000001 ) count++; // 答えとプレイヤーの入力が丸め込み誤差の１０倍より小さかったら正解とする。本当はa+b = playerとしたかった。
 	}
 
-	return count;
+	return count; // 正解数を返り値として返す。
 }
 
