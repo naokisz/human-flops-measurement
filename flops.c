4ccb457470d8ba5527d7e44c5a39f928f24f78d6
naokisz <naokihirokitomoki@gmail.com>
2015-09-14 23:55:58 +0900

    変数名の最適化。コメントの最適化。
    本屋でコーディングに関する本を読んでそれに習って少しはやってみた。
    コメントは減ったが、分かりやすくなったと思う。

--- flops.c
+++ flops.c
@@ -1,28 +1,33 @@
 #include <stdio.h>
 #include <sys/time.h> // to use gettimeofday :33,35
-#include "mondai.c" // to use mondai() :34,21
+#include "question.c" // to use question() :34,21
 
 int main(void) {
 
-	int count = 0, rensyu; // countは正解数、rensyuは練習するかのフラグ。
-	struct timeval start, end; // to use gettimeofday()
+	int numof_correct_answers = 0, if_practice; // numof_correct_answersは正解数。
+	struct timeval start_ms, end_ms; // to use gettimeofday()
 
 	printf("スーパーコンピューターと計算能力比べ!!!\n");
 
 	for (;;) {
+
 		printf("練習をするためには１、測定をするためには２を入力してください。＞");
-		scanf("%1d", &rensyu);
+		scanf("%1d", &if_practice);
 		while(getchar() != '\n') { } // 余計な値捨て
-		if(rensyu == 1) { // 練習を選んだ時
+
+		if(if_practice == 1) { 
 			printf("練習は４問です。\n");
 			printf("練習を開始するためにはエンターキーを押してください。＞");
 			rewind(stdin);
 			getchar();
-			mondai(4); // in mondai.c 4問問題作成
+
+			question(4); // in question.c 4問問題作成
+
 			printf("これで練習は終了です。\n");
 		}
-		else if(rensyu == 2) break; // 測定したい時　これだけループ脱出
-		else printf("もう一度入力してください。\n"); // 間違った入力をした時の再入力用。
+		else if(if_practice == 2) break; 
+
+		else printf("もう一度入力してください。\n"); 
 	}
 
 	printf("測定は１０問です。\n");
@@ -30,18 +35,34 @@ int main(void) {
 	rewind(stdin);
 	getchar();
 
-	gettimeofday(&start, NULL); // 始めた時間取得
-	count = mondai(10); // in mondai.c １０問の問題作成。
-	gettimeofday(&end, NULL); // 終わった時間取得
+	gettimeofday(&start_ms, NULL); // 始めた時間取得
+	numof_correct_answers = question(10); // in question.c １０問の問題作成。
+	gettimeofday(&end_ms, NULL); // 終わった時間取得
 
 	printf("これで測定は終了です。\n");
-	printf("正解数　%d問！！\n", count);
-	printf("時間　%.15lf秒！！\n", ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)*1.0E-6)); // countで正解数表示。時間は秒単位でもマイクロ秒単位でもそれぞれ終わった時間から始めた時間を引いて,それぞれを足す。マイクロ秒の方の*1.0E-6は意味不明。
-	printf("あなたのFLOPS　%.15lfFLOPS！！\n", ((double)count) / ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)*1.0E-6)); // １秒あたりの正解数　＝　FLOPS
 
-	if (((double)count) / ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)*1.0E-6) != 0) { // 0除算の例外処理のため
-		printf("東北大のスーパーコンピューターの1/%ld\n", (long)(707000000000000 / (((double)count) / ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)*1.0E-6)))); // 東北大のスパコンとの比較
-		printf("京の1/%ld\n", (long)(10000000000000000 / (((double)count) / ((end.tv_sec - start.tv_sec) + (end.tv_usec - start.tv_usec)*1.0E-6)))); // 京との比較
+	printf("正解数　%d問！！\n", numof_correct_answers);
+
+	/*時間は秒単位でもマイクロ秒単位でもそれぞれ終わった時間から始めた時間を引いて,それぞれを足す。マイクロ秒の方の*1.0E-6は意味不明。*/
+	printf("時間　%.15lf秒！！\n", 
+		((end_ms.tv_sec - start_ms.tv_sec) + (end_ms.tv_usec - start_ms.tv_usec)*1.0E-6)); 
+
+	/*１秒あたりの正解数　＝　FLOPS*/
+	printf("あなたのFLOPS　%.15lfFLOPS！！\n", 
+		((double)numof_correct_answers) / ((end_ms.tv_sec - start_ms.tv_sec) + (end_ms.tv_usec - start_ms.tv_usec)*1.0E-6)); 
+
+	/*0除算例外処理のため*/
+	if (((double)numof_correct_answers) / ((end_ms.tv_sec - start_ms.tv_sec) + (end_ms.tv_usec - start_ms.tv_usec)*1.0E-6) != 0) {
+
+		/*東北大のスパコンとの比較*/
+		/*TODO:この辺りprintfの引数などが横に長く連なっているので修正したい。*/
+		printf("東北大のスーパーコンピューターの1/%ld\n", 
+			(long)(707000000000000 / (((double)numof_correct_answers) / ((end_ms.tv_sec - start_ms.tv_sec) + (end_ms.tv_usec - start_ms.tv_usec)*1.0E-6))));
+
+		/* 京との比較*/
+		printf("京の1/%ld\n", 
+			(long)(10000000000000000 / (((double)numof_correct_answers) / ((end_ms.tv_sec - start_ms.tv_sec) + (end_ms.tv_usec - start_ms.tv_usec)*1.0E-6)))); 
+
 	} else printf("あなたのFLOPSが０のため、スーパーコンピューターと比較できません。\n"); // 0除算の例外処理
 
 	return 0;
---
+++ question.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h> // to use fabs :33
+
+/* int question
+
+引数　int型　問題数
+返り値　int型　回答数
+
+答えが0や10にならないように、整数部分は一の位、
+小数部分は小数第一位までの数(0.1~4.9)を乱数で作って、問題を出し、正解数を数える。
+
+*/
+
+int question(int numof_question) { 
+
+	int i_question, numof_correct_answers; 
+	double computer_add1, computer_add2, player_input; 
+
+	/* 乱数の種まき　mainでやるべきだろうけれど面倒だからここに書いた。*/
+	srand(time(NULL)); 
+
+	/* numof_questionの分だけ問題作成*/
+	for (i_question = 0; i_question < numof_question; i_question++) { 
+
+		/*答えを１０や０にしたくないから1から４９までの乱数に制限。*/
+		computer_add1 = rand()%49+1;
+		computer_add2 = rand()%49+1;
+
+		computer_add1 /= 10; 
+		computer_add2 /= 10;
+
+		printf("%.1lf+%.1lf=", computer_add1, computer_add2); 
+		scanf("%3lf", &player_input); 
+		while(getchar() != '\n') {  } // 余計な値捨て
+
+		/* 答えとプレイヤーの入力が丸め込み誤差の１０倍より小さかったら正解とする。
+			本当はa+b = player_inputとしたかった。*/
+		if(fabs( (computer_add1 + computer_add2) - player_input) < 0.00000000000001 )
+			numof_correct_answers++; 
+	}
+
+	return numof_correct_answers; 
+}
+
