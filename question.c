e767a3ea024b868c772fafb87c1ef709853dfd65
naokisz <naokihirokitomoki@gmail.com>
2016-04-03 17:03:45 +0900

    include<time.h>追加、少し精度を上げてみた

--- question.c
+++ question.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h> // to use fabs
+#include <time.h>
 
 /* int question
 
@@ -12,33 +13,34 @@
 
 */
 
-int question(int numof_question) { 
+int question(int numof_question) {
 
-	int i_question, numof_correct_answers = 0; 
-	double computer_add1, computer_add2, player_input = 0; 
+	int i_question, numof_correct_answers = 0;
+	double computer_add1, computer_add2, player_input = 0;
 
 	/* 乱数の種まき　mainでやるべきだろうけれど面倒だからここに書いた。*/
-	srand(time(NULL)); 
+	srand(time(NULL));
 
 	/* numof_questionの分だけ問題作成*/
-	for (i_question = 0; i_question < numof_question; i_question++) { 
+	for (i_question = 0; i_question < numof_question; i_question++) {
 
 		/*答えを１０や０にしたくないから1から４９までの乱数に制限。*/
 		computer_add1 = rand()%49+1;
 		computer_add2 = rand()%49+1;
 
-		computer_add1 /= 10; 
+		computer_add1 /= 10;
 		computer_add2 /= 10;
 
-		printf("%.1lf+%.1lf=", computer_add1, computer_add2); 
-		scanf("%3lf", &player_input); 
+		printf("%.1lf+%.1lf=", computer_add1, computer_add2);
+		scanf("%3lf", &player_input);
 		while(getchar() != '\n') {  } // 余計な値捨て
 
 		/* 答えとプレイヤーの入力が丸め込み誤差の１０倍より小さかったら正解とする。
 			本当はa+b = player_inputとしたかった。*/
 		if(fabs( (computer_add1 + computer_add2) - player_input) < 0.00000000000001 )
-			numof_correct_answers++; 
+			numof_correct_answers++;
 	}
 
-	return numof_correct_answers; 
+	return numof_correct_answers;
 }
+
